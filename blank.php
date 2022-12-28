e2383aa11d23be9c7e425470cfee5f7b20172c0f
hinglaj02 <95147773+hinglaj02@users.noreply.github.com>
2022-12-28 16:00:46 +0530

    first commit

---
+++ blank.php
@@ -0,0 +1,18 @@
+<?php
+include 'inc/config.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- <?php include 'inc/seo.php' ?> -->
+  <?php include 'inc/css.php' ?>
+</head>
+
+<body>
+  <div class="content-wrapper">
+    <?php include 'inc/header.php' ?>
+    <!-- /header -->
+</body>
+
+</html>
---
+++ inc/css.php
@@ -0,0 +1,25 @@
+
+<!-- Font Awesome Icons CSS -->
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<!-- Themify Icons CSS -->
+<link rel="stylesheet" href="css/themify-icons.css">
+<!-- Elegant Font Icons CSS -->
+<link rel="stylesheet" href="css/elegant-font-icons.css">
+<!-- Elegant Line Icons CSS -->
+<link rel="stylesheet" href="css/elegant-line-icons.css">
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<!-- Venobox CSS -->
+<link rel="stylesheet" href="css/venobox/venobox.css">
+<!-- OWL-Carousel CSS -->
+<link rel="stylesheet" href="css/owl.carousel.css">
+<!-- Slick Nav CSS -->
+<link rel="stylesheet" href="css/slicknav.min.css">
+<!-- Css Animation CSS -->
+<link rel="stylesheet" href="css/css-animation.min.css">
+<!-- Nivo Slider CSS -->
+<link rel="stylesheet" href="css/nivo-slider.css">
+<!-- Main CSS -->
+<link rel="stylesheet" href="css/main.css">
+<!-- Responsive CSS -->
+<link rel="stylesheet" href="css/responsive.css">
